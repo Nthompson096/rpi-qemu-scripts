2f23f26ddbc07c59126c777e0a5a0338775d226e
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-06-01 21:57:08 -0400

    Update instrpi-c.sh

--- instrpi-c.sh
+++ instrpi-c.sh
@@ -6,6 +6,34 @@ echo "The installer will proceed to install the RPI virtual machine with just th
 if [ ! -d /usr/share/rpi ]; then
 mkdir /usr/share/rpi 2> /dev/null && cp ./* /usr/share/rpi
 fi
+
+#Will download the RPI image here. Will ask if you want to re-extract the img even if fully downloaded.
+
+#ask if I'd want to download a version of RPI
+
+#Links
+# https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2022-01-28/2022-01-28-raspios-bullseye-arm64-lite.zip
+# 
+echo "Which RPI would you like to download?"
+
+echo "Which install, this will overwrite your virtual so please uninstall first if trying to reinstall."
+select yn in "Buster (non-graphic)" "Bullseye (non-graphic)" "Cancel"; do
+    case $yn in
+        'Buster (non-graphic)' ) echo "Choosing console install for Buster RPI" 
+        wget -c 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip' -P /var/lib/libvirt/images/ &&
+		unzip /var/lib/libvirt/images/2021-03-04-raspios-buster-armhf-lite.zip -d /var/lib/libvirt/images/ && 
+		mv /var/lib/libvirt/images/2021-03-04-raspios-buster-armhf-lite.img /var/lib/libvirt/images/rpi.img; break;;
+
+		'Bullseye (non-graphic)' ) echo "Choosing console install for Bullseye RPI"  
+        wget -c 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2022-01-28/2022-01-28-raspios-bullseye-armhf-lite.zip' -P /var/lib/libvirt/images/ &&
+		unzip /var/lib/libvirt/images/2022-01-28-raspios-bullseye-armhf-lite.zip -d /var/lib/libvirt/images/ && 
+		mv /var/lib/libvirt/images/2022-01-28-raspios-bullseye-armhf-lite.img /var/lib/libvirt/images/rpi.img; break;;
+		
+		Cancel ) echo "Ok, goodbye."; exit;;
+	esac
+done
+
+
 		if [ ! -f /usr/bin/rpistart ]; then
 		echo "Creating a start commands for virsh" && ln -sf /usr/share/rpi/rpistart.sh /usr/bin/rpistart 
 		else
@@ -34,33 +62,6 @@ fi
 		[ -f /usr/bin/rpicon ]
 		fi
 		#end
-
-#Will download the RPI image here. Will ask if you want to re-extract the img even if fully downloaded.
-
-#ask if I'd want to download a version of RPI
-
-#Links
-# https://downloads.raspberrypi.org/raspios_lite_arm64/images/raspios_lite_arm64-2022-01-28/2022-01-28-raspios-bullseye-arm64-lite.zip
-# 
-echo "Which RPI would you like to download?"
-
-echo "Which install, this will overwrite your virtual so please uninstall first if trying to reinstall."
-select yn in "Buster (non-graphic)" "Bullseye (non-graphic)" "Cancel"; do
-    case $yn in
-        'Buster (non-graphic)' ) echo "Choosing console install for Buster RPI" 
-        wget -c 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2021-03-25/2021-03-04-raspios-buster-armhf-lite.zip' -P /var/lib/libvirt/images/ &&
-		unzip /var/lib/libvirt/images/2021-03-04-raspios-buster-armhf-lite.zip -d /var/lib/libvirt/images/ && 
-		mv /var/lib/libvirt/images/2021-03-04-raspios-buster-armhf-lite.img /var/lib/libvirt/images/rpi.img; break;;
-
-		'Bullseye (non-graphic)' ) echo "Choosing console install for Bullseye RPI"  
-        wget -c 'https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2022-01-28/2022-01-28-raspios-bullseye-armhf-lite.zip' -P /var/lib/libvirt/images/ &&
-		unzip /var/lib/libvirt/images/2022-01-28-raspios-bullseye-armhf-lite.zip -d /var/lib/libvirt/images/ && 
-		mv /var/lib/libvirt/images/2022-01-28-raspios-bullseye-armhf-lite.img /var/lib/libvirt/images/rpi.img; break;;
-		
-		Cancel ) echo "Ok, goodbye."; exit;;
-	esac
-done
-
 
 # if [ -f /var/lib/libvirt/images/2021-12-02-raspios-buster-armhf.zip ] || [ ! -f /var/lib/libvirt/images/2021-12-02-raspios-buster-armhf.zip ]; then
 # wget -c 'https://downloads.raspberrypi.org/raspios_oldstable_armhf/images/raspios_oldstable_armhf-2021-12-02/2021-12-02-raspios-buster-armhf.zip' -P /var/lib/libvirt/images/ &&
